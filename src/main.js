2f58fb2d2b73bc6953d72a35ca26b9bfda419cef
Felipe Pina <fop.net@gmail.com>
2019-08-28 13:13:45 -0300

    directives...

--- src/main.js
+++ src/main.js
@@ -1,5 +1,24 @@
 import Vue from "vue";
-import App from "./App.vue";
+// import App from "./App.vue";
+import App from "./components/Form.vue";
+
+Vue.directive("highlight", {
+  bind(el, binding, vnode) {
+    // el.style.backgroundColor = 'green';
+    // el.style.backgroundColor = binding.value;
+    let delay = 0;
+    if (binding.modifiers.delayed) {
+      delay = 3000;
+    }
+    setTimeout(() => {
+      if (binding.args) {
+        el.style[args] = binding.value;
+      } else {
+        el.style.backgroundColor = binding.value;
+      }
+    }, delay);
+  },
+});
 
 export const eventBus = new Vue({
   methods: {
